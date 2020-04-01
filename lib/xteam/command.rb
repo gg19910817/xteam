020c0b534de2224821f54282a29d7692ac52f137
gg19910817 <gg19910817@gmail.com>
2020-04-01 11:34:33 +0800

    删除不必要的内容，精简代码

--- lib/xteam/command.rb
+++ lib/xteam/command.rb
@@ -1,43 +1,21 @@
 require 'colored2'
 require 'claide'
-require 'molinillo/errors'
-
-module Molinillo
-  class ResolverError
-    include CLAide::InformativeError
-  end
-end
 
 module Xteam
-  class PlainInformative
-    include CLAide::InformativeError
-  end
 
   class Command < CLAide::Command
 
-    require_relative("./command/create/iOS.rb")
+    require_relative('./command/create/iOS.rb')
 
     self.abstract_command = true
     self.command = 'xteam'
     self.version = VERSION
-    self.description = 'XTeam Tools'
-    self.plugin_prefixes = %w(claide xteam)
+    self.description = 'XTeam 官方管理工具'
 
     def self.options
       [
-        ['--silent', 'Show nothing'],
-      ].concat(super)
-    end
-
-    def self.run(argv)
-      help! 'You cannot run xteam as root.' if Process.uid == 0 && !Gem.win_platform?
-
-      super(argv)
-
-    end
-
-    def initialize(argv)
-      super
+        ['--tips', '关键操作醒目提示'],
+      ]
     end
 
   end
