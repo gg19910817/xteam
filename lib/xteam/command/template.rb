b7c6e037ef76e0c81e0b176aad7cfd305f613af5
gg19910817 <gg19910817@gmail.com>
2020-04-02 21:00:23 +0800

    目录分割

--- lib/xteam/command/template.rb
+++ lib/xteam/command/template.rb
@@ -22,14 +22,16 @@ module Xteam
             end
 
             def run
-                project_folder = "./#{@name}"
+                @project_folder = "./#{@name}"
                 # 先判断是否存在指定文件夹
-                unless Dir.exist? project_folder
+                unless Dir.exist? @project_folder
                     puts "文件夹不存在".red
                     return
                 end
 
-                if File.exist? project_folder + "/Podfile"
+                @project_name = @name.split("/").last
+
+                if File.exist? @project_folder + "/Podfile"
                     puts "发现Podfile".yellow
                     @type = :swift
                 end
@@ -50,22 +52,22 @@ module Xteam
                 todays_date = Time.now.strftime "%m/%d/%Y"
                 todays_year = Time.now.year.to_s
 
-                ios_path = "../../../../snippets/ios/"
-                kind = ask_with_answers("输入模版", ["Table", "Collection", "Scroll"]).to_s
+                ios_path = File.expand_path("../../../../snippets/ios/", __FILE__)
+                kind = ask_with_answers("输入模版", ["Table", "Collection", "Scroll"]).to_sym
                 case kind
                 when :table
-                    snippets_path = ios_path + "TABLE"
+                    snippets_path = ios_path + "/TABLE"
                 when :collection
-                    snippets_path = ios_path + "COLLECTION"
+                    snippets_path = ios_path + "/COLLECTION"
                 when :scroll
-                    snippets_path = ios_path + "SCROLL"
+                    snippets_path = ios_path + "/SCROLL"
                 else
                 end
 
-                @name = ask("输入名称")
+                @name = ask("输入名称").capitalize
 
                 string_replacements = {
-                    "TEMPLATE" => @name,
+                    "TABLE" => @name,
                     "PROJECT_OWNER" => project_owner,
                     "OWNER_TEAM" => owner_team,
                     "TODAYS_DATE" => todays_date,
@@ -73,16 +75,24 @@ module Xteam
                 }
 
                 # 这里从所有文件中替换掉模版中的名称
-                Dir.glob(snippets_path).each do |name|
+                puts snippets_path
+                Dir.foreach(snippets_path) do |name|
                     next if Dir.exists? name
-                    text = File.read(name)
+                    text = File.read(File.join(snippets_path, name))
 
                     for find, replace in string_replacements
                         text = text.gsub(find, replace)
                     end
 
-                    File.open(name, "w") { |file| file.puts text }
+                    dir_path = File.join(@project_folder, @project_name, "Pages", @name)
+                    if Dir.exists? dir_path
+                        puts "项目中已经存在指定Page".red
+                        return
+                    end
+                    Dir.mkdir(dir_path)
+                    File.open(File.join(dir_path, name.gsub("TABLE", @name)), "w") { |file| file.puts text }
                 end
+
             end
 
             def ask(question)
